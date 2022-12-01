2d290b2e40d756bfb8f02e266e4ecfe1c45abdbb
Saturn56 <S.DeepPowder@gmail.com>
2022-12-01 15:05:07 +0100

    add React Test

---
+++ React_Test/admin-panel/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './App.css';
+
+class Index extends React.Component {
+    render() {
+        return (
+        <div className="App">
+            <header className="App-header">
+                <div className=""></div>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+        </div>
+        )
+    }
+}
+export default Index;
